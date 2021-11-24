51eceeeb4cf5aa4e402d7ba9aa4c91cc5a046dd5
LenYx24 <65910507+LenYx24@users.noreply.github.com>
2021-11-24 17:54:55 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace montyhallpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int wins = 0;
+            int range = 10000000;
+            for (int i = 0; i < range; i++)
+            {
+                string[] doors = {"kecske","kecske","kecske"};
+                switch (rnd.Next(1,4)){
+                    case 1:{doors[0] = "auto"; break;}
+                    case 2:{doors[1] = "auto"; break;}
+                    case 3:{doors[2] = "auto"; break;}
+                }
+                int valasztas = rnd.Next(1,4);
+                for (int j = 1; j <= 3; j++)
+                    if(valasztas != j && doors[j-1] == "kecske") doors[j-1] = "null"; // A megmutatott ajtó kiesik
+                for (int j = 1; j <= 3; j++)
+                    if(valasztas != j && doors[j-1] != "null") valasztas = j;
+                if(doors[valasztas-1] == "auto") wins++;
+            }
+            Console.WriteLine($"wins: {wins}, percent: {float.Parse(wins.ToString()) / range}");
+        }
+    }
+}
